869a406b613dd319bc726e3f2498d1bad79ab2a8
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-06-12 22:21:24 -0300

    Update changelog-tab.js

--- js/components/changelog-tab.js
+++ js/components/changelog-tab.js
@@ -2,7 +2,7 @@ Vue.component("changelog-tab", {
     template: `<div class="changelog-tab">
     <guide-item>
     <template v-slot:title>v1.0.0</template>
-    <template v-slot:text>When modding, edit this when you make new releases!
+    <template v-slot:text>when do get huh what go release changelogs new unlockeds!
     </template>
     </guide-item>
 </div>`
