a63eb0ff3373b00c63272e94044dff00487d75ec
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-06-20 12:06:33 -0300

    Create guide_idle-tab.js

---
+++ js/components/guide_idle-tab.js
@@ -0,0 +1,17 @@
+Vue.component("guide_idle-tab", {
+    template: `<div class="guide_idle-tab">
+    <guide-item>
+    <template v-slot:title>forruf</template>
+    <template v-slot:text>you have 0 alpha
+    </template>
+    <guide-item>
+    <template v-slot:title>one</template>
+    <template v-slot:text>you have 1 alpha
+    </template>
+    <guide-item>
+    <template v-slot:title>two</template>
+    <template v-slot:text>you have 2 alpha
+    </template>
+    </guide-item>
+</div>`
+})
